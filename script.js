588e20d4e83bf837b2c80fc6dd7ebefa87cc1795
Whal0 <guciek348@gmail.com>
2024-10-29 02:29:48 +0100

    deploy new changes

--- script.js
+++ script.js
@@ -9,6 +9,29 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
         alert('Dziękujemy za wysłanie wiadomości!');
         this.reset();
     } else {
-        alert('Proszę uzupełnić wszystkie pola.');
+        alert('Wysłano pustą wiadomość');
     }
 });
+
+// Zegar
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+// Dynamicznych elementów
+
+function toggleClock() {
+    const clock = document.getElementById('clock');
+    if (clock.style.display === "none") {
+        clock.style.display = "block";
+    } else {
+        clock.style.display = "none";
+    }
+}
